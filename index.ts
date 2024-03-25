955bcaf066242aedad863f49e6dc4478c4721cdc
gfufugf <161580223+gfufugf@users.noreply.github.com>
2024-03-26 04:10:16 +0500

    created cli guessing game

---
+++ index.ts
@@ -0,0 +1,24 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+
+// 1) Computer Will Generate a Random Number
+
+// 2) User input for guessing number
+
+// 3) Compare user input with computer generated number and show result
+
+const randomNumber = Math.floor(Math.random() * 10 + 1);
+
+const answer = await inquirer.prompt([
+{
+    name: "userGuessedNumber",
+    type: "number",
+    message: "Pleas guess a number brtween 1-10:",
+},
+]);
+if(answer.userGuessedNumber === randomNumber){
+    console.log("Congragulations! you guessed right number");
+}else{
+    console.log("You guessed wrong number");
+}
